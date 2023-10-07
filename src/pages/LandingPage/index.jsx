b86fa6f6544dab3a9b582d42f5880b49bf6ddf2a
vinay <vinay.shermauryan@gmail.com>
2023-10-07 12:32:56 +0530

    fixed landing page

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -5,31 +5,7 @@ import "./style.css";
 const LandingPage = () => {
   return (
     <>
-      {/* <Box className="landing-text">
-        <Typography className="intro-text" variant="h6body1" color={"primary"}>
-          Crafting forms
-        </Typography>
-        <Typography className="bold-title" variant="h1" color={"primary"}>
-          Coming Soon
-        </Typography>
-        <Typography variant="h6" color={"primary"} className="right-text">
-          Offering customized design
-          <br /> components using advanced 3D
-          <br /> printing for architectural projects
-        </Typography>
-      </Box> */}
-
-      {/* <Box className="overlay"></Box> */}
-
-      <Box
-        className="image-container"
-        // style={{
-        //   backgroundImage: `url(${bg})`,
-        //   backgroundSize: "cover",
-        //   backgroundPosition: "center",
-        //   minHeight: "100vh",
-        // }}
-      >
+      <Box className="image-container">
         <Box className="landing-text">
           <Typography className="intro-text" variant="body1" color={"primary"}>
             Crafting forms
